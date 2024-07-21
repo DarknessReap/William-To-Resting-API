5b340fdbd4266ec28affefcc431e3e31ad0c8038
DarknessReap <wto9@myseneca.ca>
2024-07-21 12:56:05 -0400

    Update Main.js

--- Resting API/Main.js
+++ Resting API/Main.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
                 data: { term: term },
                 headers: {
                     'X-RapidAPI-Key': '1f05a6bed0msh0358b1e81fd9cf9p1e3786jsn63dec353bef9',
-                    'X-RapidAPI-Host': 'urban-dictionary7.p.rapidapi.com'
+                    'X-RapidAPI-Host': 'mashape-community-urban-dictionary.p.rapidapi.com'
                 },
                 success: function(response) {
                     if (response.list && response.list.length > 0) {
