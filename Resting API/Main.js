28058266bab35d6b7583c7ed9aa0c69e54064373
DarknessReap <wto9@myseneca.ca>
2024-07-21 13:20:06 -0400

    Y

--- Resting API/Main.js
+++ Resting API/Main.js
@@ -3,34 +3,6 @@ $(document).ready(function() {
         var term = $('#term').val();
         if (term) {
             $.ajax({
-                url: 'https://mashape-community-urban-dictionary.p.rapidapi.com/define',
-                type: 'GET',
-                data: { term: term },
-                headers: {
-                    'X-RapidAPI-Key': 'qq6oJtDbyfnXNmNZog7etxOk857bDaHrFksphcbX',
-                    'X-RapidAPI-Host': 'mashape-community-urban-dictionary.p.rapidapi.com'
-                },
-                success: function(response) {
-                    if (response.list && response.list.length > 0) {
-                        $('#definition').text(response.list[0].definition);
-                    } else {
-                        $('#definition').text('No definition found.');
-                    }
-                },
-                error: function() {
-                    $('#definition').text('Error retrieving definition.');
-                }
-            });
-        } else {
-            $('#definition').text('Please enter a term.');
-        }
-    });
-});
-$(document).ready(function() {
-    $('#search').on('click', function() {
-        var term = $('#term').val();
-        if (term) {
-            $.ajax({
                 url: 'https://www.dictionaryapi.com/api/v3/references/thesaurus/json/' + term,
                 type: 'GET',
                 data: { key: 'qq6oJtDbyfnXNmNZog7etxOk857bDaHrFksphcbX' },
