4d6c48e9cc1cd7deed3573da66bec15dc63bceaf
DarknessReap <wto9@myseneca.ca>
2024-07-24 17:55:42 -0400

    update

--- Resting API/Main.js
+++ Resting API/Main.js
@@ -5,16 +5,51 @@ $(document).ready(function() {
             $.ajax({
                 url: 'https://www.dictionaryapi.com/api/v3/references/thesaurus/json/' + term,
                 type: 'GET',
-                data: { key: 'qq6oJtDbyfnXNmNZog7etxOk857bDaHrFksphcbX' },
+                data: { key: '729bc70b-040d-4073-91c8-482b3cc5a9d2' },
                 success: function(response) {
-                    if (response.length > 0 && response[0].meta) {
-                        $('#definition').html('<strong>' + response[0].hwi.hw + ':</strong> ' + response[0].shortdef[0]);
-                        if (response[0].meta.syns && response[0].meta.syns.length > 0) {
-                            $('#definition').append('<br><strong>Synonyms:</strong> ' + response[0].meta.syns[0].join(', '));
+                    var definitionHtml = '';
+
+                    if (response.length > 0) {
+                        var entry = response[0];
+
+                        if (entry.hwi && entry.hwi.hw) {
+                            definitionHtml += '<strong>' + entry.hwi.hw + ':</strong> ';
+                        }
+
+                        if (entry.shortdef && entry.shortdef.length > 0) {
+                            definitionHtml += entry.shortdef[0];
+                        }
+
+                        if (entry.meta && entry.meta.syns && entry.meta.syns.length > 0) {
+                            definitionHtml += '<br><strong>Synonyms:</strong> ' + entry.meta.syns[0].join(', ');
+                        }
+
+                        if (entry.def && entry.def.length > 0) {
+                            entry.def.forEach(function(defEntry) {
+                                if (defEntry.sseq && defEntry.sseq.length > 0) {
+                                    defEntry.sseq.forEach(function(sseqEntry) {
+                                        var sense = sseqEntry[0][1];
+                                        if (sense.dt && sense.dt.length > 0) {
+                                            sense.dt.forEach(function(dtEntry) {
+                                                if (dtEntry[0] === 'text') {
+                                                    definitionHtml += '<br><strong>Definition:</strong> ' + dtEntry[1];
+                                                }
+                                            });
+                                        }
+
+                                        if (sense.syn_list && sense.syn_list.length > 0) {
+                                            var syns = sense.syn_list[0].map(function(syn) { return syn.wd; });
+                                            definitionHtml += '<br><strong>Additional Synonyms:</strong> ' + syns.join(', ');
+                                        }
+                                    });
+                                }
+                            });
                         }
                     } else {
-                        $('#definition').text('No definition found.');
+                        definitionHtml = 'No definition found.';
                     }
+
+                    $('#definition').html(definitionHtml);
                 },
                 error: function() {
                     $('#definition').text('Error retrieving definition.');
